2865c19925e8e7e26a90c891689fde412860abea
Kongo Abdoul <malicktakykongo@gmail.com>
2022-12-20 19:02:23 +0000

    Update the file 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,18 @@
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(*str);
-		str = str + 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+		{
+			continue;
+		}
 	}
 	_putchar('\n');
 }
